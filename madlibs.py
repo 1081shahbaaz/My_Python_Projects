fe96146c6dbd23eb761b6017dfb22b475d00ed89
Shahbaaz Mansuri <shahbaazmansuri@Shahbaazs-MacBook-Air.local>
2022-01-21 15:55:06 +0530

    MadLibs here

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#youtuber=input(enter your channel name)
+#youtuber="shahbaaz"
+
+#print(f"subscribe to {youtuber}")
+#print("subscribe to" +" "+ youtuber)
+adj = input("adjective: ")
+verb1=input("verb1: ")
+verb2=input("verb2: ")
+famous_person=input("famous person : ")
+
+madlib= f"computer programming is so {adj}! it makes me so excited all the times because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!" 
+
+print(madlib)
